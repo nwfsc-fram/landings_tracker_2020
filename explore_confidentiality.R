b32934db2fa2daca204d13e7de35666ac5bb8f23
ashley.vizek <ashley.vizek@noaa.gov>
2020-04-03 13:40:35 -0700

    remove unneeded files

--- explore_confidentiality.R
+++
@@ -1,7 +0,0 @@
-# This script pulls data and formats the dataframe for use in the app #
-library(dplyr)
-library(reshape2)
-library(lubridate)
-
-comp_dat_raw <- readRDS('comp_dat_raw.RDS') %>%
-  rename(YEAR = LANDING_YEAR)
