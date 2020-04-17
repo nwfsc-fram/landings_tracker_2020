cdd185bce88c80f45e8c6faaa71a31795d398242
ashley.vizek <ashley.vizek@noaa.gov>
2020-04-17 09:49:48 -0700

    gray lines to background

--- server.R
+++ server.R
@@ -209,8 +209,11 @@ shinyServer(function(input, output, session) {
             axis.title.x = element_blank(),
             axis.title.y = element_text(size = 12)) +
       scale_x_date(date_labels = '%b', date_breaks = "1 month") +
+      geom_line(data = filter(filtered(), Type == '2014-2019'), size = 0.6,
+                mapping = aes(color = Type)) +
       geom_line(data = filter(filtered(), !is.na(Value) & Type != '2014-2019'), linetype = 'dotted') +
-      geom_line(mapping = aes(color = Type), size = 0.6) +
+      geom_line(data = filter(filtered(), Type != '2014-2019'),
+                mapping = aes(color = Type), size = 0.6) +
       geom_point(data = filter(filtered(), no_pts == 0),
                  mapping = aes(color = Type), size = 1.5) +
       facet_wrap(~ylab, scales = 'free_y', ncol = 2) +
