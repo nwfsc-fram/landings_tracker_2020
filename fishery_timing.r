4fc5445a4313e3d191e24a476e1a02a377487571
Erin <erin.steiner@noaa.gov>
2020-04-19 21:11:54 -0700

    Add code for a fishery timing figure

---
+++ fishery_timing.r
@@ -0,0 +1,49 @@
+comp_dat_covid_app <- readRDS("comp_dat_covidapp.RDS") 
+
+dat <- subset(comp_dat_covid_app,
+  Statistic == 'Total' &
+    Metric == 'Exvessel revenue' & 
+    Cumulative == 'N' &
+    Interval == 'Monthly' &
+    Type == '2014-2019')
+
+dat_summ <- group_by(dat, State, Species, ylab, LANDING_MONTH) %>%
+  summarize(Value = sum(Value)) %>%
+  ungroup() %>%
+  group_by(State, Species, ylab) %>%
+  mutate(prop = Value/sum(Value, na.rm = T)) %>%
+  mutate(date_end = as.Date(LANDING_MONTH)+
+      months(1)) %>%
+  ungroup() %>%
+  mutate(ylab = paste0(State, ": ", Species))
+
+subset(dat_summ, grepl('California', ylab)) %>%
+ggplot(aes(x = LANDING_MONTH, y = ylab)) +
+  geom_segment(aes(x = LANDING_MONTH, y = ylab, 
+    xend = date_end, yend = ylab, #size = prop, 
+    color = prop), size = 2) +
+  scale_color_viridis() +
+  scale_x_date(date_labels = '%b', date_breaks = "1 month") +
+  ylab('') +
+  xlab('')
+
+subset(dat_summ, grepl('Oregon', ylab)) %>%
+ggplot(aes(x = LANDING_MONTH, y = ylab)) +
+  geom_segment(aes(x = LANDING_MONTH, y = ylab, 
+    xend = date_end, yend = ylab, #size = prop, 
+    color = prop), size = 2) +
+  scale_color_viridis() +
+  scale_x_date(date_labels = '%b', date_breaks = "1 month") +
+  ylab('') +
+  xlab('')
+
+subset(dat_summ, grepl('Washington', ylab)) %>%
+ggplot(aes(x = LANDING_MONTH, y = ylab)) +
+  geom_segment(aes(x = LANDING_MONTH, y = ylab, 
+    xend = date_end, yend = ylab, #size = prop, 
+    color = prop), size = 2) +
+  scale_color_viridis() +
+  scale_x_date(date_labels = '%b', date_breaks = "1 month") +
+  ylab('') +
+  xlab('')
+  
