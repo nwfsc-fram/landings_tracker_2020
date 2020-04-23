e0baeaeb3a8d19e9329d03960790a9f101d8fce5
Erin <erin.steiner@noaa.gov>
2020-04-23 10:49:21 -0700

    Add state-level fishery analysis

--- completeness.R
+++ completeness.R
@@ -18,7 +18,7 @@ for(f in 1:length(thefiles)) {
     mutate(source = file_name)
 
   alldat <- rbind(alldat, datRaw)
-  print(table(alldat$source))
+  #print(table(alldat$source))
   
 }
 
@@ -36,3 +36,35 @@ ggplot(subset(addl, LANDING_DATE > ymd('2020-03-14')), aes(x = LANDING_DATE, y =
   facet_grid(AGENCY_CODE ~ ., scales = 'free_y')
 
     
+
+wadat <- NULL
+
+for(f in 1:length(thefiles)) {
+  
+  file_name = thefiles[f]
+  print(file_name)
+  datRaw <- readRDS(file_name) %>%
+    subset(LANDING_YEAR == 2020 & AGENCY_CODE == 'W') %>%
+    group_by(LANDING_YEAR, LANDING_MONTH, LANDING_DATE, SPECIES_GROUP) %>%
+    summarise(N_vss = length(unique(VESSEL_NUM)),
+      N_proc = length(unique(DEALER_NUM)),
+      REV = sum(EXVESSEL_REVENUE),
+      MTS = sum(ROUND_WEIGHT_MTONS)) %>%
+    mutate(source = file_name)
+
+  wadat <- rbind(wadat, datRaw)
+  #print(table(wadat$source))
+  
+}
+
+addlwa <- wadat %>%
+  ungroup() %>%
+  group_by(source, SPECIES_GROUP) %>%
+  mutate(cumREV = cumsum(REV),
+    cumMTS = cumsum(MTS)) %>%
+  mutate(pull_date = ymd(gsub('.RDS', '', gsub('comp_dat_raw', '', source))))
+
+ggplot(subset(addlwa, LANDING_DATE > ymd('2020-03-14') & SPECIES_GROUP != 'SHELLFISH'), aes(x = LANDING_DATE, y = cumMTS)) + 
+  geom_line(aes(color = source)) + 
+  facet_wrap(~SPECIES_GROUP, scales = 'free_y')
+
